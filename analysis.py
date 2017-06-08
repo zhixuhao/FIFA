b4ecf0a3f55c5a6e82957690a38c1800ac8506a5
zhixuhao <zhixuhao@163.com>
2017-06-08 11:07:59 +0800

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import skimage.io as io
+
+path = ['/home/zhixuhao/Documents/FIFA/npydata/','/home/zhixuhao/Documents/FIFA/npydata/all7/']
+pathr = '/home/zhixuhao/Documents/FIFA/FIFA/'
+path_dir = ['/home/zhixuhao/Documents/FIFA/analysis/last4/','/home/zhixuhao/Documents/FIFA/analysis/all7/']
+res = ['out_last4.npy','out_all7.npy']
+
+for i in range(2):
+  test_img = np.load(path[i] + 'img_test.npy')
+  test_label = np.load(path[i] + 'img_test_label.npy')
+  test_res = np.load(pathr + res[i])
+  for j in range(len(test_label)):
+    if(test_label[j] != test_res[j]):
+      print "image",j
+      img = test_img[j,:,:,:]
+      io.imsave(path_dir[i] + str(j) + '.jpg',img)
+      
