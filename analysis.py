0ac309b0165b537f9becc0c8492198bc13e2672a
zhixuhao <zhixuhao@163.com>
2017-06-08 11:14:56 +0800

    Create analysis.py

--- analysis.py
+++ analysis.py
@@ -1,12 +1,13 @@
 import numpy as np
 import skimage.io as io
 
-path = ['/home/zhixuhao/Documents/FIFA/npydata/','/home/zhixuhao/Documents/FIFA/npydata/all7/']
+path = ['/home/zhixuhao/Documents/FIFA/npydata/','/home/zhixuhao/Documents/FIFA/npydata/all_7split/']
 pathr = '/home/zhixuhao/Documents/FIFA/FIFA/'
 path_dir = ['/home/zhixuhao/Documents/FIFA/analysis/last4/','/home/zhixuhao/Documents/FIFA/analysis/all7/']
 res = ['out_last4.npy','out_all7.npy']
 
 for i in range(2):
+    print "loading data..."
     test_img = np.load(path[i] + 'img_test.npy')
     test_label = np.load(path[i] + 'img_test_label.npy')
     test_res = np.load(pathr + res[i])
